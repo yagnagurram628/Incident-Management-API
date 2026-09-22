e8c8f4b08873ce9554f378bdfe220424b0d874af
Yagna Gurram <yagnagurram628@gmail.com>
2026-09-22 16:42:49 +0530

    Return multiple incidents

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ app = Flask(__name__)
 
 @app.route('/incidents', methods=['GET'])
 def incidents():
-    incident = {
+    incident = [{
         'id': 1,
         'title': "Payment",
         'severity': "HIGH"
-    }
+    },
+    {
+        'id': 2,
+        'title': "Server",
+        'severity': "CRITICAL"
+    },
+    {
+        'id': 3,
+        'title': "Backend",
+        'severity': "LOW"
+    }]
+
     return jsonify(incident)
 
 if __name__=="__main__":

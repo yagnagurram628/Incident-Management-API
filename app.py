f3cf2e7b838c2a8b2280ae38e337801615293687
Yagna Gurram <yagnagurram628@gmail.com>
2026-09-23 14:54:31 +0530

    handle  JSON data in POST requests using Postman

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/incidents', methods=['GET'])
+@app.route('/incidents', methods=['GET','POST'])
 def incidents():
-    incident = [{
-        'id': 1,
-        'title': "Payment",
-        'severity': "HIGH"
-    },
-    {
-        'id': 2,
-        'title': "Server",
-        'severity': "CRITICAL"
-    },
-    {
-        'id': 3,
-        'title': "Backend",
-        'severity': "LOW"
-    }]
-
+    if request.method == 'GET':
+        incident = [{
+            'id': 1,
+            'title': "Payment",
+            'severity': "HIGH"
+        },
+        {
+            'id': 2,
+            'title': "Server",
+            'severity': "CRITICAL"
+        },
+        {
+            'id': 3,
+            'title': "Backend",
+            'severity': "LOW"
+        }]
+    elif request.method == 'POST':
+            data = request.json
+            return jsonify(data)
+            
     return jsonify(incident)
 
-if __name__=="__main__":
+if __name__=="__main__": 
     app.run(debug=True)
